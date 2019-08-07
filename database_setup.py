c6530813c3ff999674c94457027ec906a75ae6d1
Adel Lassag <adetech.aadl@gmail.com>
2019-08-07 18:01:20 +0100

    feat: Complete Setting up database model

--- database_setup.py
+++ database_setup.py
@@ -11,4 +11,31 @@ class Genre(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(20), nullable=False)
-    description = Column(String(250), nullable=False)
+    description = Column(String(250))
+
+
+class Music(Base):
+    __tablename__ = 'music'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
+    artist = Column(String(100), nullable=False)
+    image = Column(String(100))
+    description = Column(String(250))
+
+    genre_id = Column(Integer, ForeignKey('genre.id'))
+    genre = relationship(Genre)
+
+    @property
+    def serialize(self)
+        return {
+            'id' : self.id,
+            'title' : self.title,
+            'artist' : self.artist,
+            'image' : self.image,
+            'genre_id' : self.genre_id
+        }
+        
+
+engine = create_engine('sqlite:///music.db')
+Base.metadata.create_all(engine)
